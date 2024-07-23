25e6ab8d48660efe7ba717da5774f83a8c629023
R19G <guseynov_rustam2017@mail.ru>
2024-07-23 16:44:36 +0300

    Update module2_5.py

--- module2_5.py
+++ module2_5.py
@@ -4,7 +4,10 @@ def get_matrix(n, m, value):  #  объявляем функцию и задае
         matrix.append([])     #  добавляем список
         for j in range(m):
             matrix[i].append(value)  # заполняем список значениями
-    print(matrix)
+    return(matrix)
 result_1 = get_matrix(2,2,10)
 result_2 = get_matrix(3, 5, 42)
 result_3 = get_matrix(4, 2, 13)
+print(result_1)
+print(result_2)
+print(result_3)
